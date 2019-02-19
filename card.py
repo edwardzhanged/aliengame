2dd0c4ce1b91e2e3b938b9e579700e683bd79bd0
Edward Zhang <45360012+edwardzhanged@users.noreply.github.com>
2019-02-19 23:15:09 +0800

    Update card.py

--- card.py
+++ card.py
@@ -1,9 +1,12 @@
 import random
+
 class blackjack:
-    def __init__(self, number_picks, decision):
+    global cards
+    global cards_picked
+    def __init__(self, number_picks,decision):
         self.number_picks = number_picks
-        self.value=value
-    def pick_cards(self):
+        self.decision=decision
+    def get_value(self):
         cards=['a','a','a','a','2','2','2','2','3','3','3','3','4','4','4','4','5','5','5',
                '5','6','6','6','6','7','7','7','7','8','8','8','8','9',
                '9','9','9','10','10','10','10','j','j','j','j','q','q','q','q','k','k','k','k']
@@ -13,15 +16,15 @@ class blackjack:
             ran_position =random.randint(1, len(cards))
             cards_picked.append(cards[ran_position])
             del cards[ran_position]
-        return cards_picked,cards
-        global  cards_picked,cards
-    def get_value(self):
+        print(cards_picked)
         value=[]
-        for m in self.number_picks:
-            if cards_picked[m]=='j'or'q'or'k':
+        for m in range(self.number_picks):
+            if cards_picked[m]=='j' or cards_picked[m]=='q' or cards_picked[m]=='k':
+                a=cards_picked[m]
                 value_transfer=10
                 value.append(value_transfer)
-            elif cards_picked[m]=='2'or'3'or'4'or'5'or'6'or'7'or'8'or'9'or'10':
+            elif cards_picked[m]=='2'or cards_picked[m]=='3'or cards_picked[m]=='4' or cards_picked[m]=='5'or cards_picked[m]=='6'or \
+                    cards_picked[m]=='7'or cards_picked[m]=='8'or cards_picked[m]=='9'or cards_picked[m]=='10':
                 value_transfer=int(cards_picked[m])
                 value.append(value_transfer)
             elif cards_picked[m]=='a':
@@ -32,4 +35,6 @@ class blackjack:
                     value_transfer=11
                     value.append(value_transfer)
         return value
-                    
+
+a=blackjack(4,'1')
+print(a.get_value())
